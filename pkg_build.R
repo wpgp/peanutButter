8c0fca6ddc511739f80da2c8c4acfdb03a6405dc
Doug Leasure <doug.leasure@gmail.com>
2020-04-13 19:57:03 +0100

    update jelly vignette

--- pkg_build.R
+++ pkg_build.R
@@ -4,19 +4,23 @@ rm(list=ls()); gc(); cat("\014"); try(dev.off(), silent=T)
 # working directory
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
+## BUILD PACKAGE
+
 # package documentation
 devtools::document()
 
 # vignettes
 devtools::build_vignettes()
 
-# about jelly
+# about tab for peanutButter::jelly
 file.copy('doc/jelly.html','inst/jelly/www/about.html', overwrite=T)
 
-# install package
+## INSTALL PACKAGE
+
+# install from source
 install.packages(getwd(), repo=NULL, type='source')
 
-# load package
+# load
 library(peanutButter)
 
 # run app
