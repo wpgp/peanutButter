be26440ad398946658df94ff1f73d25633bbd7b4
Doug Leasure <doug.leasure@gmail.com>
2020-04-19 20:22:42 +0100

    Update script.R

--- wd/code/script.R
+++ wd/code/script.R
@@ -1,56 +1,30 @@
 # cleanup
 rm(list=ls()); gc(); cat("\014"); try(dev.off(), silent=T)
 
+# packages
+library(tmap)
+
 # working directory
 setwd(file.path(dirname(rstudioapi::getSourceEditorContext()$path),'..'))
 
-# libraries
-library(sansModel)
+# source directory
+srcdir <- '//worldpop.files.soton.ac.uk/worldpop/Projects/WP517763_GRID3/Working/git/peanutButter'
 
-# load building count raster
-bldg_raster <- raster::raster('in/GHA_building_count.tif')
+# run app
+peanutButter::jelly(srcdir)
 
-# total buildings
-total_buildings <- raster::cellStats(bldg_raster, stat='sum')
+# load data
+urban <- raster::raster(file.path(srcdir,'GHA_urban.tif'))
 
-# number of sims
-n <- 1e3
+plot(urban, col=c('green','red'))
 
-# residential probability
-mu_res_prob <- 0.5
-unc_res_prob <- 0.005
-res_prob <- rbeta(n, mu_res_prob/unc_res_prob, (1-mu_res_prob)/unc_res_prob)
-hist(res_prob, main='Residential Probability', xlab='probability')
+tm_shape(tmaptools::read_osm(urban)) + 
+  
+  tm_rgb()
 
-# residential buildings
-residential_buildings <- rbinom(n, 
-                                size = total_buildings,
-                                prob = res_prob)
-
-# housing units per building
-mu_units <- 0.1
-unc_units <- 1
-units <- rtpois(n, 
-                lambda = rgamma(n, mu_units^2/unc_units^2, mu_units/unc_units^2),
-                min = 1)
-hist(units, main=paste0('Housing Units\nmean=',mean(units)), xlab='housing units / building')
-mean(units)
-
-# people per housing unit
-mu_pph <- 4
-unc_pph <- 0.2
-pph <- rtpois(n,
-              lambda = rgamma(n, mu_pph^2/unc_pph^2, mu_pph/unc_pph^2),
-              min = 1)
-hist(pph, main='Household Size', xlab='people / housing unit')
-mean(pph)
-
-# deterministic population total
-total_pop0 <- total_buildings * mu_res_prob * mu_units * mu_pph
-
-# stochastic population total
-total_pop1 <- total_buildings * mean(res_prob) * mean(units) * mean(pph)
-
-# stochastic population total
-total_pop2 <- residential_buildings * units * pph
-mean(total_pop2)
+tm_shape(urban) + 
+  
+  tm_raster(style = "cat", 
+            n = 2, 
+            title = "Settlement Type",
+            palette = c("darkolivegreen4","red"))
